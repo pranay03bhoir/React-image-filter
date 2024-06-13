d16cb531637db94dc7ce182bf86866d9d46ca5ab
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-06-13 10:03:06 +0530

    react app which filters images

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { GalleryData } from "./GalleryData";
+import { useEffect, useState } from "react";
+function App() {
+  const [data, setData] = useState([]);
+  const [collection, setCollection] = useState([]);
+
+  useEffect(() => {
+    setData(GalleryData);
+    setCollection([...new Set(GalleryData.map((item) => item.titile))]);
+  }, []);
+
+  const gallery_filter = (itemData) => {
+    const filterData = GalleryData.filter((item) => item.titile == itemData);
+    setData(filterData);
+  };
+
+  return (
+    <div className="App">
+      <div className="galleryWrapper">
+        <div className="filterItem">
+          <ul>
+            <li>
+              <button onClick={() => setData(GalleryData)}>All</button>
+            </li>
+            {collection.map((item) => (
+              <li>
+                <button
+                  onClick={() => {
+                    gallery_filter(item);
+                  }}
+                >
+                  {item}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="galleryContainer">
+          {data.map((item) => (
+            <div key={item.id} className="galleryItem">
+              <img src={item.image} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/GalleryData.js
@@ -0,0 +1,64 @@
+export const GalleryData = [
+  {
+    id: 1,
+    titile: "iconic moments",
+    image: "./images/i1.jpeg",
+  },
+  {
+    id: 2,
+    titile: "iconic moments",
+    image: "./images/i2.jpeg",
+  },
+  {
+    id: 3,
+    titile: "iconic moments",
+    image: "./images/i3.jpg",
+  },
+  {
+    id: 4,
+    titile: "iconic moments",
+    image: "./images/i4.jpeg",
+  },
+  {
+    id: 5,
+    titile: "Special Moments",
+    image: "./images/s1.jpeg",
+  },
+  {
+    id: 6,
+    titile: "Special Moments",
+    image: "./images/s2.jpeg",
+  },
+  {
+    id: 7,
+    titile: "Special Moments",
+    image: "./images/s3.jpeg",
+  },
+  {
+    id: 8,
+    titile: "Special Moments",
+    image: "./images/s4.jpeg",
+  },
+
+  {
+    id: 9,
+    titile: "Awards & Recognition",
+    image: "./images/a1.jpeg",
+  },
+  {
+    id: 10,
+    titile: "Awards & Recognition",
+    image: "./images/a2.jpeg",
+  },
+  {
+    id: 11,
+    titile: "Awards & Recognition",
+    image: "./images/a3.jpeg",
+  },
+  {
+    id: 12,
+    titile: "Awards & Recognition",
+    image: "./images/a4.jpeg",
+  },
+  
+];
